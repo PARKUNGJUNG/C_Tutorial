f0e69584703950d4882efbb07842fffa19551a1b
박정웅 <lgchu10@naver.com>
2025-03-17 10:37:06 +0900

    초기 커밋.

---
+++ 2025-03-10/변수와 대입문.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int x, y, z;
+	x = 10;
+	y = 10 + 20;
+	z = 30 * 40;
+	printf("%d,%d\n", x, y);
+
+	int a, b;
+	a = 5;
+	b = 7;
+	int c, d;
+	c = a * b;
+	d = a * b - 10;
+	printf("a=%d b=%d c=%d d=%d", a, b, c, d);
+
+	return 0;
+}
+
