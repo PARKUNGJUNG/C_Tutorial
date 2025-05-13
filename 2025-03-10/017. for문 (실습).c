10c2a6bb059118c2a0c9d5c8602d804f7dc27522
박정웅 <lgchu10@naver.com>
2025-05-13 23:07:36 +0900

    2025-05-13 23:07

--- 2025-03-10/017. for문 (실습).c
+++ 2025-03-10/017. for문 (실습).c
@@ -17,7 +17,7 @@ int main(void) {
 		}
 		printf("%d\n", sum);
 	}
-	*/
+	
 
 	{
 		///실습문제2
@@ -42,15 +42,45 @@ int main(void) {
 		///실습문제2
 		//2)
 		int stor = 0;
-		int d, n, k;
+		int stor1 = 0;
+		int k, d, n;
 			printf("k d n = ");
 			scanf_s("%d%d%d", &k, &d, &n);
-			printf("A1 = %d", k);
-		for (int i = 2; i <= n; i++) {
+			printf("A1 = %d\n", k);
 			int m = k + d;
-			printf("A%d = %d\n", i, stor);
 			stor += m;
-
+			printf("A2 = %d\n", m);
+			for (int i = 3; i <= n; i++) {
+				int m1 = stor + d;
+				stor = m1;
+					printf("A%d = %d\n", i, m1);
 		}
+	}
+	*/
+
+	{
+		///실습문제3
+		for (int i = 1; i <= 15; i++) {
+			if (i % 2 == 0) { continue; }
+			printf("%d ", i);
+		}
+		printf("\n");
+
+		for (int i = 10; i >= 1; i--) {
+			printf("%d ", i);
+		}
+		printf("\n");
+
+		for (int i = 4; i <= 32; i++) {
+			if (i % 4 != 0) { continue; }
+			printf("%d ", i);
+		}
+		printf("\n");
+
+		for (int i = 4; i <= 32; i++) {
+			if (i % 4 != 0) { continue; }
+			printf("%d ", i);
+		}
+		printf("\n");
 	}
 }
