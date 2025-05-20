259b15c8992f3afc732093b48b4e1db71864b3dd
박정웅 <lgchu10@naver.com>
2025-05-20 11:10:14 +0900

    2025-05-20 11:10

---
+++ 2025-03-10/019. 다중 for문 (실습).c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+
+void n_sum(void) {
+	int n;
+	int sum = 0;
+	printf("n = ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	printf("%d", sum);
+}
+
+int main(void) {
+
+	/*
+	{
+		///실습문제1
+		for (int i = 2; i <= 8; i += 2) {
+			printf("<구구단 %d단>\n", i);
+			for (int j = 1; j <= 9; j++) {
+				printf("%d * %d = %2d\n", i, j, i * j);
+
+			}
+			printf("\n");
+		}
+	}
+	
+
+	{
+		///실습문제2
+		//1)
+		int n;
+		int sum = 0;
+		printf("n = ");
+		scanf_s("%d", &n);
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		printf("%d", sum);
+	}
+	
+
+	{
+		///실습문제2
+		//2)
+		int n, m;
+		int sum = 0;
+		printf("m = ");
+		scanf_s("%d", &m);
+		for (int i = 1; i <= m; i++) {
+			printf("n = ");
+			scanf_s("%d", &n);
+			for (int i = 1; i <= n; i++) {
+				sum += i;
+			}
+			printf("%d\n", sum);
+			sum = 0;
+		}
+	}
+	
+
+	{
+		///실습문제2
+		//3)
+		n_sum();
+	}
+	*/
+
+	{
+		///실습문제3
+		//1)
+		int n, m;
+		printf("n m = ");
+		scanf_s("%d%d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				printf("1");
+			}
+			printf("\n");
+		}
+
+		//2)
+		n, m = 0;
+		int cnt = 0;
+		printf("n m = ");
+		scanf_s("%d%d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				cnt++;
+				printf("%d", cnt);
+			}
+			cnt = 0;
+			printf("\n");
+		}
+
+		//3)
+		n, m = 0;
+		cnt = 0;
+		printf("n m = ");
+		scanf_s("%d%d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				cnt++;
+				printf("%02d ", cnt);
+			}
+			printf("\n");
+		}
+
+		//4)
+		n, m = 0;
+		int k = 6;
+		printf("n m = ");
+		scanf_s("%d%d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= k; j++) {
+				printf("%02d ", j);
+			}
+			k++;
+			printf("\n");
+		}
+
+		//5)
+		n, m = 0;
+		printf("n m = ");
+		scanf_s("%d%d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				printf("%d", j);
+			}
+			printf("\n");
+		}
+	}
+}
