27e3c0cae88e832c50ddfff66b855c35220baad6
박정웅 <lgchu10@naver.com>
2025-06-10 11:42:26 +0900

    2025-06-10 11:42

--- 2025-03-10/022. 자료형.c
+++ 2025-03-10/022. 자료형.c
@@ -121,6 +121,16 @@ int main(void) {
 	//대입문에서 정보 손실 가능성이 있는 자동 형변환
 	//- 표현 범위가 더 큰 자료형 데이터 -> 표현 범위가 더 작은 자료형 변수
 
+	///연습문제
+	{
+		int kor, eng, math;
+		printf("국어 영어 수학>>");
+		scanf_s("%d%d%d", &kor, &eng, &math);
+		int tot = kor + eng + math;
+		double avg = (double)tot / 3;
+		printf("총점은 %d점, 평균은 %.2f점 입니다.\n", tot, avg);
+
+	}
 
 
 
